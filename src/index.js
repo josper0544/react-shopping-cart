aa6f730e58fd6bfc75f761bdce3f832d1886de65
Geoff Gredden <g.gredden@greddentek.com>
2020-05-04 14:23:01 +1000

    bare bones react shopping cart

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const products = [
+    {
+        "id" : 1,
+        "name" : "mango",
+        "price" : 10
+    },
+    {
+        "id" : 2,
+        "name" : "apple",
+        "price": 5
+    }];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App products={products} />, document.getElementById("root"));
